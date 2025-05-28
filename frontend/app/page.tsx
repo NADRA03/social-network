118dfb5e34a0a469b180147d4c48174d6180bf5d
zalhaj <zahraalhaj27@gmail.com>
2025-05-29 00:09:42 +0300

    initial setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black-100 text-2xl font-bold">
+      Welcome to Your Social Network 🚀
+    </main>
+  );
+}
